62e3ac06136c053308173017630fecee3af20088
Mihai N <74415486+devneagu@users.noreply.github.com>
2021-07-20 10:23:15 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./styles.css";
 import * as dataJson from "./data.json";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function find(arr, name, parent) {
   return arr.find((el) => el[name] === parent);
 }
 export default function App() {
-  console.log(dataJson);
+  const [data, setData] = useState([]);
   const w = [];
   useEffect(() => {
     dataJson.values.forEach((item) => {
       var emotionItem = item.replaceAll(" ", "");
       var emotionItemArray = emotionItem.split("-");
-
       if (
         !w.some(
           (el) =>
@@ -26,12 +25,61 @@ export default function App() {
         });
       }
     });
-    console.table(w);
+
+    let uniqueEmotions = [...new Set(w.map((item) => item.emotion))];
+    uniqueEmotions = uniqueEmotions.map((item) => {
+      return {
+        name: item,
+        children: []
+      };
+    });
+    uniqueEmotions.forEach((unique, idx) => {
+      var flag = 0;
+      w.forEach((item, idx2) => {
+        if (flag === 1) {
+          uniqueEmotions[idx]["children"][
+            uniqueEmotions[idx]["children"].length - 1
+          ]["children"].push({ name: item.name });
+          flag = 0;
+        }
+        if (item.emotion === unique.name) {
+          if (
+            unique.children.filter((el) => el.name === item.parent).length === 0
+          ) {
+            uniqueEmotions[idx]["children"].push({
+              name: item.parent,
+              children: [{ name: item.name }]
+            });
+            flag = 1;
+          }
+        }
+      });
+    });
+
+    console.log(uniqueEmotions);
+    setData(uniqueEmotions);
   }, []);
+  console.log(data);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      {data.length &&
+        data.map((item) => (
+          <div key={item.name} className="emotionParent">
+            {item.name}
+            <ul>
+              {item.children.map((item2) => (
+                <>
+                  <li>{item2.name}</li>
+                  <ul>
+                    {item2.children.map((item3) => (
+                      <li>{item3.name}</li>
+                    ))}
+                  </ul>
+                </>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </>
   );
 }
